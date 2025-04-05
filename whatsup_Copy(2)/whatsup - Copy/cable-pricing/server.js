98f8469091e5860deb73dbe3237c802d2d7cfb9f
ashishu703 <149085363+ashishu703@users.noreply.github.com>
2025-04-05 15:11:41 +0530

    Update server.js

--- whatsup_Copy(2)/whatsup - Copy/cable-pricing/server.js
+++ whatsup_Copy(2)/whatsup - Copy/cable-pricing/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
+const PORT = 3004;
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/cablePricing', {
